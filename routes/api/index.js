d0a010369e013c50715a71bdf4bc95c701e02796
Contrapuntist <contrapuntist@gmail.com>
2017-10-11 22:19:55 -0500

    routes not triggering an error now on app load

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router(); 
-// const petBreedMatchRoutes = require('./petbreedmatch'); 
-// const petBreedinfoRoutes = require('./petbreedinfo');
+const petBreedMatchRoutes = require('./petbreedmatch'); 
+const petBreedinfoRoutes = require('./petbreedinfo');
 const userRoutes = require('./user');
 const authRoutes = require('./auth');
 
-// router.use('/petbreedmatch', petBreedMatchRoutes); 
-// router.use('/petbreed', petbreedRoutes);
+router.use('/petbreedmatch', petBreedMatchRoutes); 
+router.use('/petbreedinfo', petBreedinfoRoutes);
 router.use('/signup', authRoutes);
 router.use('/newuser', userRoutes);
 

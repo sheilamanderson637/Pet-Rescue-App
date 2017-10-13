5c5f68acb50c26e967d8cd66b78f87881972e49a
liz-rondone <liz.rondone@northwestern.edu>
2017-10-13 14:24:22 -0500

    home js edits

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./pages/Home";
-import NoMatch from "./pages/NoMatch";
-import Footer from "./components/Footer";
-import Home from './pages/Home';
-import Hometemp from './components/temphome';
 import Header from './components/Header';
+import Home from './pages/Home';
+import NoMatch from "./pages/NoMatch";
+import Hometemp from './components/temphome';
 import Registration from './pages/Registration';
+// import Footer from "./components/Footer";
 
 const App = () =>
   <Router>
     <div>
-      <Header />
+      <Header component={Header} />
       <Switch>
         <Route exact path="/" component={Home} /> 
         <Route component={NoMatch} />
         <Route exact path='/' component={Hometemp} /> 
         <Route exact path='/registration'  component={Registration}/>
       </Switch>
-      <Footer />
+      {/* <Footer component={Footer} /> */}
     </div>
   </Router>
 

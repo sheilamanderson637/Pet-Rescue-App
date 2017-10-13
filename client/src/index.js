572e407b9521409404f937f4e628ce2f877e6fc2
John O'Connell <johnoconnell@Johns-MacBook-Pro.local>
2017-10-13 11:02:27 -0500

    deog rendering and modal files

--- client/src/index.js
+++ client/src/index.js
@@ -4,5 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import App from "./JohnApp";
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+

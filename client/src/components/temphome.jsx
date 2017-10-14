28aa6a3fb487fe802938911325b61c8072765d91
Contrapuntist <contrapuntist@gmail.com>
2017-10-14 00:17:03 -0500

    got petfinder api to pull results via console

--- client/src/components/temphome.jsx
+++ client/src/components/temphome.jsx
@@ -1,13 +1,35 @@
 import React from 'react'; 
+import Petfinder from '../utils/petfinderapi';
 
 
+
+let testObj = { 
+    age: 'young',
+    sex: 'male',
+    size: 'small', 
+    zip: '60089',
+    animaltype: 'dog',
+    breed: 'pug'
+}
+
+function getPetsToRescue(obj) { 
+    console.log(obj);
+    Petfinder(obj)
+    .then((res) => {
+        console.log(res);
+        console.log(res.data.petfinder.pets.pet)
+    }).catch((err) => console.log(err));
+}
+
+
+getPetsToRescue(testObj);
 
 const Hometemp = () => (
     <div className='container'>
-    <div className="jumbotron">
-        <h1 className="display-3">Finding Fido Temp</h1>
-        <p>temp component until home page component elements completed. </p>
-    </div>
+        <div className="jumbotron">
+            <h1 className="display-3">Finding Fido Temp</h1>
+            <p>temp component until home page component elements completed. </p>
+        </div>
     </div>
 ); 
 
--- routes/api/index.js
+++ routes/api/index.js
@@ -4,10 +4,12 @@ const petBreedMatchRoutes = require('./petbreedmatch');
 const petBreedinfoRoutes = require('./petbreedinfo');
 const userRoutes = require('./user');
 const authRoutes = require('./auth');
+const petfinderRoutes = require('./petfinder');
 
 router.use('/petbreedmatch', petBreedMatchRoutes); 
 router.use('/petbreedinfo', petBreedinfoRoutes);
 router.use('/signup', authRoutes);
 router.use('/newuser', userRoutes);
+router.use('/petfinder', petfinderRoutes);
 
 module.exports = router;

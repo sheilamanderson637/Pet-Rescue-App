e157e0c5014f941e4f6134cbad8d067b6f832684
paisley777 <jthurk@northwestern.edu git config --global user.name Your>
2017-10-15 14:33:40 -0500

    saved pet routes

--- routes/api/petbreedmatch.js
+++ routes/api/petbreedmatch.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = require('express').Router(); 
 const petbreedController = require('../../controllers/petbreedController');
 
-router.route('/')
-    .get(petbreedController.findbreedMatch);
+// router.route('/')
+//     .get(petbreedController.findbreedMatch);
 
-router.route('/all')
-    .get(petbreedController.findallBreedlookups);
+// router.route('/all')
+//     .get(petbreedController.findallBreedlookups);
 
 module.exports = router;

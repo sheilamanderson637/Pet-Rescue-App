e8b18b050946c6a57ffeef56e02e48d9a02ea318
Contrapuntist <contrapuntist@gmail.com>
2017-10-15 14:52:33 -0500

    saving state of changes for merge

--- client/src/components/Dogform/Dogform.js
+++ client/src/components/Dogform/Dogform.js
@@ -3,7 +3,7 @@ import {FormGroup, Label, Input, Col} from 'reactstrap';
 
 const Dogform = props =>
 
-<form onSubmit={this.handleSubmit}>
+<form>
   
   {/* GENDER   */}
         <FormGroup tag="fieldset" row>
@@ -11,25 +11,25 @@ const Dogform = props =>
                 <Col sm={10}>
                  <FormGroup check>
                     <Label check>
-                     <Input type="radio" value="female" name="dog-sex" class="custom-control-input" 
-                        required checked={this.state.selectedOption === 'female'} 
-                        onChange={this.handleOptionChange} />
+                     <Input type="radio" value={props.dogsex} name="dogsex" 
+                        required checked
+                        onChange={props.handleOptionChange} />{' '}
                         Female
                     </Label>
                     </FormGroup>
-                    <FormGroup>
+                    <FormGroup check>
                     <Label check>
-                     <Input type="radio" value="male" name="dog-sex" class="custom-control-input" 
-                        required checked={this.state.selectedOption === 'male'} 
-                        onChange={this.handleOptionChange} />
+                     <Input type="radio" value={props.dogsex}  name="dogsex"
+                        required checked 
+                        onChange={props.handleOptionChange} />{' '}
                         Male
                     </Label>
                     </FormGroup>
-                    <FormGroup>
+                    <FormGroup check>
                     <Label check>
-                     <Input type="radio" value="any" name="dog-sex" class="custom-control-input" 
-                        required checked={this.state.selectedOption === 'any'} 
-                        onChange={this.handleOptionChange} />
+                     <Input type="radio" value={props.dogsex} name="dogsex"
+                        required checked
+                        onChange={props.handleOptionChange} />{' '}
                         No Preference
                     </Label>
                 </FormGroup>
@@ -37,145 +37,145 @@ const Dogform = props =>
         </FormGroup>
 
  {/* AGE */}
-        <FormGroup tag="fieldset" row>
+        {/* <FormGroup tag="fieldset" row>
             <legend className="col-form-legend col-sm-2">Age</legend>
                 <Col sm={10}>
-                 <FormGroup check>
+                <FormGroup check>
                     <Label check>
-                     <Input type="radio" value="puppy" name="dog-age" class="custom-control-input" 
-                        required checked={this.state.selectedOption === 'puppy'} 
-                        onChange={this.handleOptionChange} />
+                    <Input type="radio" value="puppy" name="dog-age"
+                        required checked={props.selectedOption === 'puppy'} 
+                        onChange={props.handleOptionChange} />{' '}
                         Puppy
                     </Label>
                     </FormGroup>
                     <FormGroup>
                     <Label check>
-                     <Input type="radio" value="young" name="dog-age" class="custom-control-input" 
-                        required checked={this.state.selectedOption === 'young'} 
-                        onChange={this.handleOptionChange} />
+                    <Input type="radio" value="young" name="dog-age"
+                        required checked={props.selectedOption === 'young'} 
+                        onChange={props.handleOptionChange} />{' '}
                         Young
                     </Label>
                     </FormGroup>
                     <FormGroup>
                     <Label check>
-                     <Input type="radio" value="adult" name="dog-age" class="custom-control-input" 
-                        required checked={this.state.selectedOption === 'adult'} 
-                        onChange={this.handleOptionChange} />
+                    <Input type="radio" value="adult" name="dog-age"
+                        required checked={props.selectedOption === 'adult'} 
+                        onChange={props.handleOptionChange} />{' '}
                         Adult
                     </Label>
                     </FormGroup>
                     <FormGroup>
                     <Label check>
-                     <Input type="radio" value="senior" name="dog-age" class="custom-control-input" 
-                        required checked={this.state.selectedOption === 'senior'} 
-                        onChange={this.handleOptionChange} />
+                    <Input type="radio" value="senior" name="dog-age"
+                        required checked={props.selectedOption === 'senior'} 
+                        onChange={props.handleOptionChange} />{' '}
                         Senior
                     </Label>
                 </FormGroup>
             </Col>
-        </FormGroup>
+        </FormGroup> */}
 
 {/* SIZE */}
-        <FormGroup tag="fieldset" row>
+        {/* <FormGroup tag="fieldset" row>
             <legend className="col-form-legend col-sm-2">Size</legend>
                 <Col sm={10}>
                  <FormGroup check>
                     <Label check>
-                     <Input type="radio" value="small" name="dog-size" class="custom-control-input" 
-                        required checked={this.state.selectedOption === 'small'} 
-                        onChange={this.handleOptionChange} />
+                     <Input type="radio" value="small" name="dog-size"
+                        required checked={props.selectedOption === 'small'} 
+                        onChange={props.handleOptionChange} />{' '}
                         Small
                     </Label>
                     </FormGroup>
-                    <FormGroup>
+                    <FormGroup check>
                     <Label check>
-                     <Input type="radio" value="medium" name="dog-size" class="custom-control-input" 
-                        required checked={this.state.selectedOption === 'medium'} 
-                        onChange={this.handleOptionChange} />
+                     <Input type="radio" value="medium" name="dog-size"
+                        required checked={props.selectedOption === 'medium'} 
+                        onChange={props.handleOptionChange} />{' '}
                         Medium
                     </Label>
                     </FormGroup>
-                    <FormGroup>
+                    <FormGroup check>
                     <Label check>
-                     <Input type="radio" value="large" name="dog-size" class="custom-control-input" 
-                        required checked={this.state.selectedOption === 'large'} 
-                        onChange={this.handleOptionChange} />
+                     <Input type="radio" value="large" name="dog-size"
+                        required checked={props.selectedOption === 'large'} 
+                        onChange={props.handleOptionChange} />{' '}
                         Large
                     </Label>
                 </FormGroup>
             </Col>
-        </FormGroup>
+        </FormGroup> */}
 
 {/* HOUSING */}
-        <FormGroup tag="fieldset" row>
+        {/* <FormGroup tag="fieldset" row>
             <legend className="col-form-legend col-sm-2">Housing</legend>
                 <Col sm={10}>
                  <FormGroup check>
                     <Label check>
-                     <Input type="radio" value="home" name="dog-home" class="custom-control-input" 
-                        required checked={this.state.selectedOption === 'home'} 
-                        onChange={this.handleOptionChange} />
+                     <Input type="radio" value="home" name="dog-home"
+                        required checked={props.selectedOption === 'home'} 
+                        onChange={props.handleOptionChange} />{' '}
                         Home
                     </Label>
                     </FormGroup>
                     <FormGroup check>
                     <Label check>
-                     <Input type="radio" value="apartment" name="dog-home" class="custom-control-input" 
-                        required checked={this.state.selectedOption === 'apartment'} 
-                        onChange={this.handleOptionChange} />
+                     <Input type="radio" value="apartment" name="dog-home"
+                        required checked={props.selectedOption === 'apartment'} 
+                        onChange={props.handleOptionChange} />{' '}
                         Apartment/Condo
                     </Label>
                 </FormGroup>
             </Col>
-        </FormGroup>
+        </FormGroup> */}
 
 {/* SHEDDING */}
-        <FormGroup tag="fieldset" row>
+        {/* <FormGroup tag="fieldset" row>
             <legend className="col-form-legend col-sm-2">Shedding</legend>
                 <Col sm={10}>
                  <FormGroup check>
                     <Label check>
-                     <Input type="radio" value="light" name="dog-hair" class="custom-control-input" 
-                        required checked={this.state.selectedOption === 'light'} 
-                        onChange={this.handleOptionChange} />
+                     <Input type="radio" value="light" name="dog-hair"
+                        required checked={props.selectedOption === 'light'} 
+                        onChange={props.handleOptionChange} />{' '}
                         Light
                     </Label>
                 </FormGroup>
                 <FormGroup check>
                     <Label check>
-                     <Input type="radio" value="any" name="dog-hair" class="custom-control-input" 
-                        required checked={this.state.selectedOption === 'any'} 
-                        onChange={this.handleOptionChange} />
+                     <Input type="radio" value="any" name="dog-hair"
+                        required checked={props.selectedOption === 'any'} 
+                        onChange={props.handleOptionChange} />{' '}
                         No Preference
                     </Label>
                 </FormGroup>
             </Col>
-        </FormGroup>
+        </FormGroup> */}
 
 {/* ENERGY */}
-        <FormGroup tag="fieldset" row>
+        {/* <FormGroup tag="fieldset" row>
             <legend className="col-form-legend col-sm-2">Energy Level</legend>
                 <Col sm={10}>
                  <FormGroup check>
                     <Label check>
-                     <Input type="radio" value="highenergy" name="dog-energy" class="custom-control-input" 
-                        required checked={this.state.selectedOption === 'highenergy'} 
-                        onChange={this.handleOptionChange} />
+                     <Input type="radio" value="highenergy" name="dog-energy"
+                        required checked={props.selectedOption === 'highenergy'} 
+                        onChange={props.handleOptionChange} />{' '}
                         High
                     </Label>
                     </FormGroup>
                     <FormGroup check>
                     <Label check>
-                     <Input type="radio" value="calm" name="dog-energy" class="custom-control-input" 
-                        required checked={this.state.selectedOption === 'calm'} 
-                        onChange={this.handleOptionChange} />
+                     <Input type="radio" value="calm" name="dog-energy"
+                        required checked={props.selectedOption === 'calm'} 
+                        onChange={props.handleOptionChange} />{' '}
                         Calm
                     </Label>
                 </FormGroup>
             </Col>
-        </FormGroup>
+        </FormGroup> */}
 
-<button type="submit" id="submitbtn" class="btn btn-lg btn-warning">Submit</button>                   
- </form>
+    <button type="submit" id="submitbtn" className="btn btn-lg btn-warning" onSubmit={props.handleSubmit}>Submit</button>                   
+</form>
 
 export default Dogform;
--- client/src/pages/DogQuestionnaire/DogQuestionnaire.js
+++ client/src/pages/DogQuestionnaire/DogQuestionnaire.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Container } from 'reactstrap'
 import Dogform from '../../components/Dogform'
 import Wrapper from '../../components/Wrapper'
 import FriendCard from '../../components/FriendCard'
@@ -8,22 +9,45 @@ class DogQuestionnaire extends Component {
     constructor(props) { 
         super(props)
         this.state = { 
-            selectedOption: ''
+            dogsex: '',
+            doghair: '',
+            dogsize: '',
+            dogenergy: '',
+            doghome: '' 
         }
+
+        this.handleChange = this.handleOptionChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleOptionChange() { 
-
+    handleSubmit = event => {
+        event.preventDefault(); 
+        console.log(this.state.selectedOption);
     }
+
+    handleOptionChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
+        });
+        console.log(this.state);
+      };
+    
+
     render() { 
        return(
-        <Wrapper>
-            <Dogform  
-                selectedOption={this.state.selectedOption}
+        <div>
+          <Container>  
+            <Dogform 
+                dogsex={this.state.dogsex}
+                dogsize={this.state.dogsize} 
+                selectedOption={this.state}
                 handleOptionChange={this.handleOptionChange} 
+                submitForm={this.handleSubmit}
                 />
-            <FriendCard />
-        </Wrapper>
+            {/* <FriendCard /> */}
+            </Container>
+        </div>
        );
     }
 }

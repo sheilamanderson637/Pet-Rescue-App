95837404490857d795555b545628d32b59e0f8a2
Contrapuntist <contrapuntist@gmail.com>
2017-10-17 02:28:39 -0500

    converting petfinder results into new array of objects.  Conditioning for some needs needed when certain fields have not data.

--- client/src/components/Results/results.js
+++ client/src/components/Results/results.js
@@ -1,5 +1,4 @@
 import React from 'react'; 
-import {Jumbotron, Container} from 'reactstrap';
 import FriendCard from '../FriendCard';
 import Wrapper from '../../components/Wrapper';
 
@@ -8,34 +7,28 @@ const Results = props => {
     console.log(props.petfinderResults);
     let petcards;
     if (props.petfinderResults.length > 0) {
-        console.log('props array greater than 0');
+        petcards = props.petfinderResults[0].name;
+        console.log(petcards);
+    }
+
         petcards = props.petfinderResults.map((petcard) => {
             return(
-            <div className='card my-2' key={petcard}>
+            <div className='card my-2' key={petcard.id.$t}>
                 <div>
                     <p>New Petcard</p> 
-                    {petcard}
+                    <p>{petcard.description}</p>
                 </div>
             </div>
             );
         });
 
-    } else { 
-        console.log('props petfinder did not propogate in time');
-        petcards = <h3>Uh oh, it appears no pets are available to rescue in the zipcode you submitted.</h3>;
-    }
+    // } else { 
+    //     console.log('props petfinder did not propogate in time');
+    //     petcards = <h3>Uh oh, it appears no pets are available to rescue in the zipcode you submitted.</h3>;
+    // }
  
     return ( 
         <div style={{marginTop: 20}}>
-            {/* <Container>
-            <Jumbotron>
-                <h2>BREED NAME: {props.breedName}</h2>
-                <p className='lead'>DESCRIPTION: {props.breedDescription}</p>
-                <hr /> 
-                <p>TEMPERAMENT: {props.breedTemperament}</p>
-                <p>HISTORY: {props.breedHistory}</p>
-            </Jumbotron>
-            </Container> */}
             <Wrapper>
             {petcards}
             </Wrapper>

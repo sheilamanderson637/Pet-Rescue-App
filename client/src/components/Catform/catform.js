6ed0349fcee4e7ae84ac584c3378b9846646cc68
paisley777 <jthurk@northwestern.edu git config --global user.name Your>
2017-10-16 21:32:09 -0500

    cat form

--- client/src/components/Catform/catform.js
+++ client/src/components/Catform/catform.js
@@ -1,159 +1,69 @@
 import React from "react";
-import {FormGroup, Label, Input} from 'reactstrap';
+import {Form, FormGroup, Label, Input, Button} from 'reactstrap';
 
-const Catform = props =>
-
-<form onSubmit={this.handleSubmit}>
-  
-  {/* GENDER   */}
-        <FormGroup tag="fieldset" row>
-            <legend className="col-form-legend col-sm-2">Gender</legend>
-                <Col sm={10}>
-                 <FormGroup check>
-                    <Label check>
-                     <Input type="radio" value="female" name="cat-sex" class="custom-control-input" 
-                        required checked={this.state.selectedOption === 'female'} 
-                        onChange={this.handleOptionChange} />
-                        Female
-                    </Label>
-                    </FormGroup>
-                    <FormGroup>
-                    <Label check>
-                     <Input type="radio" value="male" name="cat-sex" class="custom-control-input" 
-                        required checked={this.state.selectedOption === 'male'} 
-                        onChange={this.handleOptionChange} />
-                        Male
-                    </Label>
-                    </FormGroup>
-                    <FormGroup>
-                    <Label check>
-                     <Input type="radio" value="any" name="cat-sex" class="custom-control-input" 
-                        required checked={this.state.selectedOption === 'any'} 
-                        onChange={this.handleOptionChange} />
-                        No Preference
-                    </Label>
-                </FormGroup>
-            </Col>
-        </FormGroup>
-
- {/* AGE */}
-        <FormGroup tag="fieldset" row>
-            <legend className="col-form-legend col-sm-2">Age</legend>
-                <Col sm={10}>
-                 <FormGroup check>
-                    <Label check>
-                     <Input type="radio" value="baby" name="cat-age" class="custom-control-input" 
-                        required checked={this.state.selectedOption === 'baby'} 
-                        onChange={this.handleOptionChange} />
-                        Kitten
-                    </Label>
-                    </FormGroup>
-                    <FormGroup>
-                    <Label check>
-                     <Input type="radio" value="young" name="cat-age" class="custom-control-input" 
-                        required checked={this.state.selectedOption === 'young'} 
-                        onChange={this.handleOptionChange} />
-                        Young
-                    </Label>
-                    </FormGroup>
-                    <FormGroup>
-                    <Label check>
-                     <Input type="radio" value="adult" name="cat-age" class="custom-control-input" 
-                        required checked={this.state.selectedOption === 'adult'} 
-                        onChange={this.handleOptionChange} />
-                        Adult
-                    </Label>
-                    </FormGroup>
-                    <FormGroup>
-                    <Label check>
-                     <Input type="radio" value="senior" name="cat-age" class="custom-control-input" 
-                        required checked={this.state.selectedOption === 'senior'} 
-                        onChange={this.handleOptionChange} />
-                        Senior
-                    </Label>
-                </FormGroup>
-            </Col>
-        </FormGroup>
-
-{/* SIZE */}
-        <FormGroup tag="fieldset" row>
-            <legend className="col-form-legend col-sm-2">Size</legend>
-                <Col sm={10}>
-                 <FormGroup check>
-                    <Label check>
-                     <Input type="radio" value="small" name="cat-size" class="custom-control-input" 
-                        required checked={this.state.selectedOption === 'small'} 
-                        onChange={this.handleOptionChange} />
-                        Small
-                    </Label>
-                    </FormGroup>
-                    <FormGroup>
-                    <Label check>
-                     <Input type="radio" value="medium" name="cat-size" class="custom-control-input" 
-                        required checked={this.state.selectedOption === 'medium'} 
-                        onChange={this.handleOptionChange} />
-                        Medium
-                    </Label>
-                    </FormGroup>
-                    <FormGroup>
-                    <Label check>
-                     <Input type="radio" value="large" name="cat-size" class="custom-control-input" 
-                        required checked={this.state.selectedOption === 'large'} 
-                        onChange={this.handleOptionChange} />
-                        Large
-                    </Label>
-                </FormGroup>
-            </Col>
-        </FormGroup>
-
-{/* FRIENDLY */}
-        <FormGroup tag="fieldset" row>
-            <legend className="col-form-legend col-sm-2">Friendly with Other Pets</legend>
-                <Col sm={10}>
-                 <FormGroup check>
-                    <Label check>
-                     <Input type="radio" value="yes" name="cat-friendly" class="custom-control-input" 
-                        required checked={this.state.selectedOption === 'yes'} 
-                        onChange={this.handleOptionChange} />
-                        Yes
-                    </Label>
-                    </FormGroup>
-                    <FormGroup check>
-                    <Label check>
-                     <Input type="radio" value="any" name="cat-friendly" class="custom-control-input" 
-                        required checked={this.state.selectedOption === 'any'} 
-                        onChange={this.handleOptionChange} />
-                        No Preference
-                    </Label>
-                </FormGroup>
-            </Col>
-        </FormGroup>
-
-{/* PLAYFUL */}
-        <FormGroup tag="fieldset" row>
-            <legend className="col-form-legend col-sm-2">Playful</legend>
-                <Col sm={10}>
-                 <FormGroup check>
-                    <Label check>
-                     <Input type="radio" value="yes" name="cat-playful" class="custom-control-input" 
-                        required checked={this.state.selectedOption === 'yes'} 
-                        onChange={this.handleOptionChange} />
-                        Yes
-                    </Label>
-                </FormGroup>
-                <FormGroup check>
-                    <Label check>
-                     <Input type="radio" value="no" name="cat-playful" class="custom-control-input" 
-                        required checked={this.state.selectedOption === 'no'} 
-                        onChange={this.handleOptionChange} />
-                        No 
-                    </Label>
-                </FormGroup>
-            </Col>
-        </FormGroup>
-
-
-<button type="submit" id="submitbtn" class="btn btn-lg btn-warning">Submit</button>                   
- </form>
+const Catform = props => { 
+return(
+    <div>
+        <Form onSubmit={props.handleSubmit}>
+            {/* GENDER */}
+            <FormGroup>
+                <Label htmlFor="catsex">What gender do you prefer for your cat?</Label>
+                <Input type='select' value={props.value} onChange={props.handleOptionChange} name='catsex' className="form-control" >
+                <option></option>
+                <option value='male'>Male</option>
+                <option value='female'>Female</option>
+                <option value='any'>Any</option>
+                </Input>
+            </FormGroup>
+            {/* AGE */}
+            <FormGroup>
+                <Label htmlFor="catage">What age cat would you like?</Label>
+                <Input type='select' value={props.value} onChange={props.handleOptionChange} name='catage' className="form-control" >
+                <option></option>
+                <option value='baby'>Baby</option>
+                <option value='young'>Young</option>
+                <option value='adult'>Adult</option>
+                <option value='senior'>Senior</option>
+                </Input>
+            </FormGroup>
+            {/* SIZE */}
+            <FormGroup>
+                <Label htmlFor="catsize">What size dog do you prefer?</Label>
+                <Input type='select' value={props.value} onChange={props.handleOptionChange} name='catsize' className="form-control" >
+                <option></option>
+                <option value='small'>Small</option>
+                <option value='medium'>Medium</option>
+                <option value='large'>Large</option>
+                </Input>
+            </FormGroup>
+            {/* FRIENDLINESS */}
+            <FormGroup>
+                <Label htmlFor="catfriendly">Does this cat need to be friendly with other pets?</Label>
+                <Input type='select' value={props.value} onChange={props.handleOptionChange} name='catfriendly' className="form-control" >
+                <option></option>
+                <option value='yes'>Yes</option>
+                <option value='no'>No</option>
+                </Input>
+            </FormGroup>
+            {/* PLAYFUL */}
+            <FormGroup>
+                <Label htmlFor="catplay">Would you like a playful cat?</Label>
+                <Input type='select' value={props.value} onChange={props.handleOptionChange} name='catplay' className="form-control" >
+                <option></option>
+                <option value='yes'>Yes</option>
+                <option value='no'>No</option>
+                </Input>
+            </FormGroup>
+            {/* ZIP */}
+            <FormGroup>
+                <Label>Zip:</Label>
+                <Input type="text" name="zip" value={props.value} 
+                onChange={props.handleOptionChange} />
+            </FormGroup>
+        <Button onSubmit={() => props.onSubmit} type='submit'>Submit </Button>
+        </Form>  
+    </div> 
+    );
+}
 
 export default Catform;
